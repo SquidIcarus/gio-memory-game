81c6fbea3bdf0fdf2eec1a79f7aabec94ab58d63
User Name <landonisaiah@gmail.com>
2025-05-09 12:24:47 -0700

    cached element references and console tests

---
+++ app.js
@@ -0,0 +1,29 @@
+
+/*-------------------------------- Constants --------------------------------*/
+const blackNotes = ['C#', 'D#', 'F#', 'G#', 'A#'];
+const whiteNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
+// console.log(whiteNotes);
+// console.log(blackNotes);
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const startBtnElem = document.getElementById('start-btn');
+const arpLengthElem = document.querySelector('.sequence-count');
+const msgElem = document.getElementById('message');
+const blackNotesElem = document.getElementById('black-keys');
+const whiteNotesElem = document.getElementById('white-keys');
+const displayNoteElem = document.getElementById('display-note');
+
+// console.log();
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
