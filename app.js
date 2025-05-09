a8085732a7dd3411fe0684c7d4368836ed334ce3
User Name <landonisaiah@gmail.com>
2025-05-09 15:23:47 -0700

    create key note buttons

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
 /*-------------------------------- Constants --------------------------------*/
-const blackNotes = ['C#', 'D#', 'F#', 'G#', 'A#'];
-const whiteNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+const pianoNotes = [
+    { note: 'C', type: 'white' },
+    { note: 'C#', type: 'black' },
+    { note: 'D', type: 'white' },
+    { note: 'D#', type: 'black' },
+    { note: 'E', type: 'white' },
+    { note: 'E#', type: 'black' },
+    { note: 'F', type: 'white' },
+    { note: 'F#', type: 'black' },
+    { note: 'G', type: 'white' },
+    { note: 'G#', type: 'black' },
+    { note: 'A', type: 'white' },
+    { note: 'A#', type: 'black' },
+    { note: 'B', type: 'white' }
+];
 
-// console.log(whiteNotes);
-// console.log(blackNotes);
+console.log(pianoNotes);
+
 
 
 /*-------------------------------- Variables --------------------------------*/
@@ -26,9 +39,16 @@ const displayNoteElem = document.getElementById('display-note');
 
 function handleStartBtn(event) {
     console.log(event);
+};
+
+function pianoSequencer() {
+    pianoNotes.forEach(noteObj => {
+        
+    })
 }
 
 
-/*----------------------------- Event Listeners -----------------------------*/
 
-startBtnElem.addEventListener('click', handleStartBtn);
+        /*----------------------------- Event Listeners -----------------------------*/
+
+        startBtnElem.addEventListener('click', handleStartBtn);
